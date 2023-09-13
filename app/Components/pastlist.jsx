3b2b7c7217a89d88fa1f1d1acc4d7f31aa126975
Jonathan Mejia <jefry.jonathan.quintanilla@gmail.com>
2023-09-13 09:44:36 -0600

    comprobation added

--- app/Components/pastlist.jsx
+++ app/Components/pastlist.jsx
@@ -21,18 +21,22 @@ const Pastlist = () => {
         transition={{ duration: 1 }}>
             <h1 className='text-2xl text-deep-blue font-bold'>Historial de Compras</h1>
             <div>
-                {historyData.map((item, index) => (
-                    <div key={index} className="historial-item text-dee">
-                    <li className="bg-beige  border-2 relative border-deep-blue text-deep-blue pt-12 justify-around flex items-center h-12vh xl:h-12vh w-full">
-                        <p className='text-md font-light'>SS/20</p>
-                        <h1 className='text-3xl text-deep-blue font-oswald tracking-tighter text-center break-words  '>{item.name}</h1>
-                        <div className='flex flex-col items-center justify-around mb-5'>
-                            <p className='text-sm font-semibold line-through' >{item.price}</p>
-                            <p className='text-xl font-semibold'>{item.discount}</p>
-                        </div>     
-                    </li>
-                    </div>
-                ))}
+                {historyData ? (
+                    historyData.map((item, index) => (
+                        <div key={index} className="historial-item text-dee">
+                            <li className="bg-beige  border-2 relative border-deep-blue text-deep-blue pt-12 justify-around flex items-center h-12vh xl:h-12vh w-full">
+                                <p className='text-md font-light'>SS/20</p>
+                                <h1 className='text-3xl text-deep-blue font-oswald tracking-tighter text-center break-words  '>{item.name}</h1>
+                                <div className='flex flex-col items-center justify-around mb-5'>
+                                    <p className='text-sm font-semibold line-through' >{item.price}</p>
+                                    <p className='text-xl font-semibold'>{item.discount}</p>
+                                </div>     
+                            </li>
+                        </div>
+                    ))
+                ) : (
+                    <div className="text-deep-blue">No hay datos disponibles.</div>
+                )}
             </div>
         </motion.section>
     );
